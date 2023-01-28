55b0a5a3cf899524c811801fb79e44d31e362d63
Sultan-assasin <sulltanadai@gmail.com>
2023-01-28 22:50:48 +0600

    added firestore and some changes in navigation graph

--- app/src/main/java/com/sultan/mvvmshop/activityes/LoginRegisterActivity.kt
+++ app/src/main/java/com/sultan/mvvmshop/activityes/LoginRegisterActivity.kt
@@ -3,7 +3,9 @@ package com.sultan.mvvmshop.activityes
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sultan.mvvmshop.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
