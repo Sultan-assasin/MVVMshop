0716811b3384a11ea70ecad7e1c54d475159cc53
Sultan-assasin <sulltanadai@gmail.com>
2023-01-30 23:42:43 +0600

    added shopping_graph.xml and worked correctly

--- app/src/main/java/com/sultan/mvvmshop/activityes/ShoppingActivity.kt
+++ app/src/main/java/com/sultan/mvvmshop/activityes/ShoppingActivity.kt
@@ -2,11 +2,21 @@ package com.sultan.mvvmshop.activityes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.sultan.mvvmshop.R
+import com.sultan.mvvmshop.databinding.ActivityShoppingBinding
 
 class ShoppingActivity : AppCompatActivity() {
+
+    val binding by lazy {
+        ActivityShoppingBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_shopping)
+        setContentView(binding.root)
+
+        val navController = findNavController(R.id.shoppingHostFragment)
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
---
+++ app/src/main/java/com/sultan/mvvmshop/fragments/shopping/ProfileFragment.kt
@@ -0,0 +1,7 @@
+package com.sultan.mvvmshop.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.sultan.mvvmshop.R
+
+class ProfileFragment: Fragment(R.layout.fragment_profile) {
+}
